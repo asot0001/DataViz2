7a32c89976209b925255c5e8dcabf893e796a54b
Ali <ali@experimacs-iMac.local>
2022-10-13 01:35:13 +1100

    Commit message

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,6 +3,7 @@ var vg_2 = "chart2.json";
 var vg_3 = "chart3.json";
 var vg_4 = "chart4.json";
 var vg_5 = "chart5.json";
+var vg_6 = "Last.json";
 vegaEmbed("#map", vg_1,{ "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -15,6 +16,6 @@ vegaEmbed("#chart3", vg_3,{ "actions": false }).then(function(result) {
 vegaEmbed("#chart4", vg_4,{ "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#chart5", vg_5,{ "actions": false }).then(function(result) {
+vegaEmbed("#Last", vg_6,{ "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
