54a3fcb8b703bf872727da92aec6df624a1c01a6
Ali <ali@experimacs-iMac.local>
2022-10-12 16:31:28 +1100

    adding files

---
+++ vega_lite_vis.js
@@ -0,0 +1,12 @@
+var vg_1 = "map.json";
+var vg_2 = "chart2.json";
+var vg_3 = "chart3.json";
+vegaEmbed("#map", vg_1,{ "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#chart2", vg_2,{ "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#chart3", vg_3,{ "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
